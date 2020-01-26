55d46a1c837aae53d49ab64ca6ed177053666a62
Lessllie Real <rlessllie@yahoo.com>
2020-01-26 05:36:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,18 +4,18 @@ katz_deli = []
 
 
 def line(array)
-if [katz_deli] < 1
-  then 
-  puts "The line is currently empty."
+#if [katz_deli] < 1
+ # then 
+  #puts "The line is currently empty."
 
 end
 
 def take_a_number(array, name)
-  puts ""
+  #puts ""
 end
 
 def now_serving
-  puts ""
+  #puts ""
   
-  puts "There is nobody waiting to be served!"
+  #puts "There is nobody waiting to be served!"
 end
