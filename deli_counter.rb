7c676f904795179046bbd9901687dab4d30ac880
Lessllie Real <rlessllie@yahoo.com>
2020-01-26 05:45:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,18 +4,30 @@ katz_deli = []
 
 
 def line(array)
+  
    #if [katz_deli] = []
   #puts "The line is currently empty."
   # end
+  
 end 
 #line[]
 
 def take_a_number(array, name)
+  
   #puts ""
+  
 end
 
 def now_serving
+  
   #puts ""
   
   #puts "There is nobody waiting to be served!"
+  
 end
+
+
+
+
+
+
