19ff06963de84a2f61e0442c5da575e4dd4fa759
Lessllie Real <rlessllie@yahoo.com>
2020-01-26 05:43:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ katz_deli = []
 
 
 def line(katz_deli)
-   if [katz_deli] = []
-  puts "The line is currently empty."
-   end
+   #if [katz_deli] = []
+  #puts "The line is currently empty."
+  # end
 end 
 line[]
 
